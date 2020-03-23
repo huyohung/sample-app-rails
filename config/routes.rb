120dddc3fce8ddcc2c4d02e260fa0cbec059b6e8
Huy Hung <hungbh@gmail.com>
2020-03-23 13:27:18 +0700

    feat: adding layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get '/home' => 'static_pages#home'
   get '/help' => 'static_pages#help'
   get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'static_pages#home'
 end
