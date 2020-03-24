3aa8106a1e01475f073cc0dc3d58be39c7aa32a9
Huy Hung <hungbh@gmail.com>
2020-03-24 15:02:00 +0700

    feat: signup function

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_063116) do
+ActiveRecord::Schema.define(version: 2020_03_24_073849) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "password"
+    t.string "password_confirmation"
   end
 
 end
