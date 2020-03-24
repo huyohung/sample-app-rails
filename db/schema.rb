0a14896afd5d0ba2cc89fe57fcf18835faba2b4e
Huy Hung <hungbh@gmail.com>
2020-03-24 15:41:52 +0700

    feat: adding validation for signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_073849) do
+ActiveRecord::Schema.define(version: 2020_03_24_083337) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_073849) do
     t.string "password_digest"
     t.string "password"
     t.string "password_confirmation"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
