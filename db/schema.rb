219a6f4c6caa2cbd7bbd731fa0e3890b7d7e535f
Huy Hung <hungbh@gmail.com>
2020-03-25 17:07:17 +0700

    feat: adding remember me function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_083337) do
+ActiveRecord::Schema.define(version: 2020_03_25_091603) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_083337) do
     t.string "password_digest"
     t.string "password"
     t.string "password_confirmation"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
