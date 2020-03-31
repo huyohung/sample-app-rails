5e03adf06ddef7f9de9af93cedc43d1eb5a9c067
Huy Hung <hungbh@gmail.com>
2020-03-31 18:06:28 +0700

    feat: send active link via Gmail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,26 +3,22 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
-  def index
-    @users = User.paginate(page: params[:page])
+  def new
+    if logged_in?
+      flash[:danger] = "You need to logout first"
+      redirect_to root_url
+    else
+      @user = User.new 
+    end
   end
 
   def show
     @user = User.find(params[:id])
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def edit
-    @user = User.find(params[:id])
+    redirect_to root_url and return unless true
   end
 
   def create
@@ -36,23 +32,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to @user
+    if change_params?
+      if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
     else
-      render 'edit'
+      redirect_to @user
     end
   end
 
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-    # Confirms a logged-in user.
   def logged_in_user
     unless logged_in?
       store_location
@@ -60,15 +57,25 @@ class UsersController < ApplicationController
       redirect_to login_url
     end
   end
-  # Confirms the correct user.
+
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   # Confirms an admin user.
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
-  
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
